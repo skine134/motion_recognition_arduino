b2e884619ac2fe428462749f3df52215f6c76374
DESKTOP-5B9VU1L\skine <skine134@naver.com>
2021-05-26 16:26:45 +0900

    코드 잘못 업로딩 한 점 수정.

--- motion_recognition.ino
+++ motion_recognition.ino
@@ -1,18 +1,50 @@
-#include <SoftwareSerial.h>
 
+#include <MotionRecognition.h>
+#include <SoftwareSerial.h>
 
 #define TX_PIN 2
 #define RX_PIN 3
-SoftwareSerial mySerial(TX_PIN, RX_PIN);
 
+#define ROTATION_SIZE 0   //0 : FULL(1-1.8), 1 : HALF(1-0.9), 2 : QUART(1-0.45) [ Hz - angle ]
+#define DELAY_TIME 0.1    // 초 단위
+
+String data;
+SoftwareSerial btSerial(TX_PIN, RX_PIN);
+MotionRecognition motionRecognition;
 void setup()
 {
-  Serial.begin(9600);
-  mySerial.begin(115200);
-  mySerial.print("Hello! \n");
+  // Serial3.begin(115200);
+  btSerial.begin(115200);
+  motionRecognition.begin(ROTATION_SIZE);
 }
 void loop()
 {
-  if(mySerial.available())
-  Serial.println((char)mySerial.read());
+  /*
+  * 주석은 Mega2560 전용
+  */
+  // if(!Serial3.available())
+  /****
+   * 
+   * read String
+   * 
+   ****/
+  if(!btSerial.available())
+    return;  
+  // char tmp = Serial3.read();
+  char tmp = btSerial.read();
+  data+=tmp;
+  int endIndex = data.indexOf("<EOF>");
+  if(endIndex == -1)
+    return;
+  /****
+   * 
+   *   yaw 측정용
+   *
+   ****/
+  //  Serial.println("yaw : "+String(inputYaw,3));
+  motionRecognition.rotationStepMoter(data.substring(1,endIndex),DELAY_TIME);
+  /***
+   * 데이터 처리후 data 초기화 
+   */
+  data="";
 }
